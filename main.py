bfc19c1fd3503f18e3239626cb2885932e29b1b4
Valentin-Semur <valentin.semur@gmail.com>
2024-09-02 14:07:24 +0200

    create the player and let it rotate

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    you = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
 
@@ -13,8 +18,11 @@ def main():
                 return
 
         screen.fill("black")
-        # pygame.display.flip()
+        you.draw(screen)
+        you.update(dt)
+        pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
